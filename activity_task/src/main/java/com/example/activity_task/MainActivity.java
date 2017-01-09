795a8ab605aa86643d7c6d991fefb929439611d7
yywishp@gmail.com <gyxyydry529914>
2017-01-09 16:41:11 +0800

    checktextview控件

--- activity_task/src/main/java/com/example/activity_task/MainActivity.java
+++ activity_task/src/main/java/com/example/activity_task/MainActivity.java
@@ -4,15 +4,18 @@ import android.app.Activity;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.CheckedTextView;
 
 public class MainActivity extends Activity {
-
+    private CheckedTextView checktextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initState();
+
     }
 
     private void initState() {
@@ -22,5 +25,17 @@ public class MainActivity extends Activity {
             //透明导航栏
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }*/
+
+
+        checktextview = (CheckedTextView) findViewById(R.id.checktextview);
+
+        checktextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checktextview.toggle();
+            }
+        });
+
+
     }
 }
