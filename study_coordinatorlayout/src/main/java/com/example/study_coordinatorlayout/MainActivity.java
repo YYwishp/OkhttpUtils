251d0a5face4c51c4d3e8f8618b73ab37b69c331
yywishp@gmail.com <gyxyydry529914>
2017-05-04 15:05:26 +0800

    study_coordinatorlayout

---
+++ study_coordinatorlayout/src/main/java/com/example/study_coordinatorlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.study_coordinatorlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
