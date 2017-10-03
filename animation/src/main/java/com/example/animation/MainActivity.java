f687efe3155c1f47abd7b3cae38548189d049437
yywishp <yywishp@gmail.com>
2017-10-04 01:09:33 +0800

    动画测试

--- animation/src/main/java/com/example/animation/MainActivity.java
+++ animation/src/main/java/com/example/animation/MainActivity.java
@@ -1,110 +1,100 @@
 package com.example.animation;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * View动画
  */
-public class MainActivity extends AppCompatActivity {
-	private TextView textview;
-
+public class MainActivity extends Activity {
+	private ImageView imgLogo;
+	private Button button;
+	private int height;
+	private final int finalHeight = 0;
+	private int measuredHeight;
+	private int realHeight;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-
-		textview = (TextView) findViewById(R.id.textview);
-
-		textview.setOnTouchListener(new View.OnTouchListener() {
-
-			int startX;
-			int startY;
-
-			@Override
-			public boolean onTouch(View view, MotionEvent event) {
-
-				switch (event.getAction()) {
-					case MotionEvent.ACTION_DOWN:
-						//按下的事件
-						Log.e("测试","按下....");
-						//1.按下控件,记录开始的x和y的坐标
-						startX = (int) event.getRawX();
-						startY = (int) event.getRawY();
-						break;
-					case MotionEvent.ACTION_MOVE:
-						//移动的事件
-						Log.e("测试","移动了....");
-						//2.移动到新的位置记录新的位置的x和y的坐标
-						//int newX = (int) event.getRawX();
-						//int newY = (int) event.getRawY();
-						//3.计算移动的偏移量
-						//int dX = newX - startX;
-						//int dY = newY - startY;
-						//4.移动相应的偏移量,重新绘制控件
-						//获取的时候原控件距离父控件左边和顶部的距离
-//						int l = textview.getLeft();
-//						int t = textview.getTop();
-//						//获取新的控件的距离父控件左边和顶部的距离
-//						//l += dX;
-//						//t += dY;
-//						int r = l + textview.getWidth();
-////						int b = t + textview.getHeight();
-//						int b = 20 + textview.getHeight();
-
-
-						//5.更新开始的坐标
-						//tartX = newX;
-						//startY = newY;
-						break;
-					case MotionEvent.ACTION_UP:
-						//抬起的事件
-					Log.e("测试","抬起了....");
-						int newY = (int) event.getRawY();
-						//3.计算移动的偏移量
-						//int dX = newX - startX;
-						int dY = newY - startY;
-						if (dY >30) {
-							trans(view,0,textview.getTop()+50);
-						} else if (dY < -30) {
-							trans2(view,0,textview.getTop()-50);
-						}
-						break;
+	
+		
+		button = (Button) findViewById(R.id.button);
+		
+		
+		
+		
+		imgLogo = (ImageView) findViewById(R.id.img_logo);
+		
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		measuredHeight = imgLogo.getMeasuredHeight();
+		
+		ViewGroup.LayoutParams layoutParams = imgLogo.getLayoutParams();
+		height = layoutParams.height;
+		realHeight = imgLogo.getHeight();
+	}
+	
+	public void startAnimation(View view) {
+		Log.e("tag","measuredHeight"+measuredHeight);
+		Log.e("tag","layoutParamsheight"+height);
+		Log.e("tag","realHeight"+realHeight);
+		
+		HiddenAnimUtils.newInstance(this,imgLogo,button,realHeight).toggle();
+	}
+	
+	public void startAnimationY(View view) {
+		if (imgLogo.getVisibility() == View.VISIBLE) {
+			ObjectAnimator scaleY = ObjectAnimator.ofFloat(imgLogo, "translationY", 0.0f, -realHeight);
+			imgLogo.setPivotX(0);
+			imgLogo.setPivotY(0);
+			imgLogo.invalidate();
+			scaleY.setDuration(1000);
+			scaleY.start();
+			scaleY.addListener(new AnimatorListenerAdapter() {
+				@Override
+				public void onAnimationEnd(Animator animation) {
+					imgLogo.setVisibility(View.GONE);
 				}
-
-
-
-
-				return true;
-			}
-		});
-
-
-	}
-
-	public void trans(View view,int start ,int end){
-		//iv.setTranslationX(translationX);
-		int i = view.getHeight() / 3;
-
-		ObjectAnimator oa = ObjectAnimator.ofFloat(view, "translationY", start,end);
-		oa.setDuration(1000);
-//		oa.setRepeatCount(ObjectAnimator.INFINITE);
-//		oa.setRepeatMode(ObjectAnimator.REVERSE);
-		oa.start();
-	}
-	public void trans2(View view,int start ,int end){
-		int i = view.getHeight() / 3;
-		//iv.setTranslationX(translationX);
-		ObjectAnimator oa = ObjectAnimator.ofFloat(view, "translationY", start,end);
-		oa.setDuration(1000);
-//		oa.setRepeatCount(ObjectAnimator.INFINITE);
-//		oa.setRepeatMode(ObjectAnimator.REVERSE);
-		oa.start();
+			});
+			
+		} else {
+			
+			imgLogo.setVisibility(View.VISIBLE);
+			ObjectAnimator scaleY = ObjectAnimator.ofFloat(imgLogo, "translationY", -realHeight, 0.0f);
+			imgLogo.setPivotX(0);
+			imgLogo.invalidate();
+			imgLogo.setPivotY(0);
+			scaleY.setDuration(1000);
+			scaleY.start();
+			scaleY.addListener(new AnimatorListenerAdapter() {
+				@Override
+				public void onAnimationEnd(Animator animation) {
+					
+				}
+			});
+		}
+		
+		
+		
+		
 	}
 }
